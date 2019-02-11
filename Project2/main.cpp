e34e3542b531e522316e410329f6e05e38267964
Rishi <rishikaley@gmail.com>
2019-02-11 16:03:08 +0530

    Splash State and MainMenu State

---
+++ Project2/main.cpp
@@ -0,0 +1,10 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Game.hpp"
+#include "DEFINITIONS.hpp"
+
+int main() {
+
+	Titans::Game(SCREEN_WIDTH, SCREEN_HEIGHT, "SFML Starter");
+	return 0;
+}
