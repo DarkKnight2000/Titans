2d945fc9b7776caa2eb2cca32388c806b50c012f
Siddharth0827 <46277902+Siddharth0827@users.noreply.github.com>
2019-02-15 21:19:27 +0530

    Update DEFINITIONS.hpp

--- Project2/DEFINITIONS.hpp
+++ Project2/DEFINITIONS.hpp
@@ -24,27 +24,39 @@
 #define DINO_WALK_ANIMATION_DURATION 1.6f
 #define DINO_WALK_SPEED 100.0f
 
-#define KNIGHT_WALK_FRAME_1_FILEPATH "Resources/res/Knight/Walk (1).png"
-#define KNIGHT_WALK_FRAME_2_FILEPATH "Resources/res/Knight/Walk (2).png"
-#define KNIGHT_WALK_FRAME_3_FILEPATH "Resources/res/Knight/Walk (3).png"
-#define KNIGHT_WALK_FRAME_4_FILEPATH "Resources/res/Knight/Walk (4).png"
-#define KNIGHT_WALK_FRAME_5_FILEPATH "Resources/res/Knight/Walk (5).png"
-#define KNIGHT_WALK_FRAME_6_FILEPATH "Resources/res/Knight/Walk (6).png"
-#define KNIGHT_WALK_FRAME_7_FILEPATH "Resources/res/Knight/Walk (7).png"
-#define KNIGHT_WALK_FRAME_8_FILEPATH "Resources/res/Knight/Walk (8).png"
-#define KNIGHT_WALK_FRAME_9_FILEPATH "Resources/res/Knight/Walk (9).png"
-#define KNIGHT_WALK_FRAME_10_FILEPATH "Resources/res/Knight/Walk (10).png"
-#define KNIGHT_WALK_ANIMATION_DURATION 1.6f
-#define KNIGHT_WALK_SPEED 100.0f
-#define KNIGHT_JUMP_FRAME_1_FILEPATH "Resources/res/Knight/Jump (1).png"
-#define KNIGHT_JUMP_FRAME_2_FILEPATH "Resources/res/Knight/Jump (2).png"
-#define KNIGHT_JUMP_FRAME_3_FILEPATH "Resources/res/Knight/Jump (3).png"
-#define KNIGHT_JUMP_FRAME_4_FILEPATH "Resources/res/Knight/Jump (4).png"
-#define KNIGHT_JUMP_FRAME_5_FILEPATH "Resources/res/Knight/Jump (5).png"
-#define KNIGHT_JUMP_FRAME_6_FILEPATH "Resources/res/Knight/Jump (6).png"
-#define KNIGHT_JUMP_FRAME_7_FILEPATH "Resources/res/Knight/Jump (7).png"
-#define KNIGHT_JUMP_FRAME_8_FILEPATH "Resources/res/Knight/Jump (8).png"
-#define KNIGHT_JUMP_FRAME_9_FILEPATH "Resources/res/Knight/Jump (9).png"
-#define KNIGHT_JUMP_FRAME_10_FILEPATH "Resources/res/Knight/Jump (10).png"
-#define KNIGHT_JUMP_ANIMATION_DURATION 1.6f
-#define KNIGHT_JUMP_SPEED 100.0f
+#define KNIGHT_RUN_FRAME_1_FILEPATH "Resources/res/Knight/Walk (1).png"
+#define KNIGHT_RUN_FRAME_2_FILEPATH "Resources/res/Knight/Walk (2).png"
+#define KNIGHT_RUN_FRAME_3_FILEPATH "Resources/res/Knight/Walk (3).png"
+#define KNIGHT_RUN_FRAME_4_FILEPATH "Resources/res/Knight/Walk (4).png"
+#define KNIGHT_RUN_FRAME_5_FILEPATH "Resources/res/Knight/Walk (5).png"
+#define KNIGHT_RUN_FRAME_6_FILEPATH "Resources/res/Knight/Walk (6).png"
+#define KNIGHT_RUN_FRAME_7_FILEPATH "Resources/res/Knight/Walk (7).png"
+#define KNIGHT_RUN_FRAME_8_FILEPATH "Resources/res/Knight/Walk (8).png"
+#define KNIGHT_RUN_FRAME_9_FILEPATH "Resources/res/Knight/Walk (9).png"
+#define KNIGHT_RUN_FRAME_10_FILEPATH "Resources/res/Knight/Walk (10).png"
+#define KNIGHT_RUN_ANIMATION_DURATION 1.6f
+#define KNIGHT_RUN_SPEED 100.0f
+#define KNIGHT_ATTACK_FRAME_1_FILEPATH "Resources/res/Knight/Jump (1).png"
+#define KNIGHT_ATTACK_FRAME_2_FILEPATH "Resources/res/Knight/Jump (2).png"
+#define KNIGHT_ATTACK_FRAME_3_FILEPATH "Resources/res/Knight/Jump (3).png"
+#define KNIGHT_ATTACK_FRAME_4_FILEPATH "Resources/res/Knight/Jump (4).png"
+#define KNIGHT_ATTACK_FRAME_5_FILEPATH "Resources/res/Knight/Jump (5).png"
+#define KNIGHT_ATTACK_FRAME_6_FILEPATH "Resources/res/Knight/Jump (6).png"
+#define KNIGHT_ATTACK_FRAME_7_FILEPATH "Resources/res/Knight/Jump (7).png"
+#define KNIGHT_ATTACK_FRAME_8_FILEPATH "Resources/res/Knight/Jump (8).png"
+#define KNIGHT_ATTACK_FRAME_9_FILEPATH "Resources/res/Knight/Jump (9).png"
+#define KNIGHT_ATTACK_FRAME_10_FILEPATH "Resources/res/Knight/Jump (10).png"
+#define KNIGHT_ATTACK_ANIMATION_DURATION 1.6f
+#define KNIGHT_ATTACK_SPEED 100.0f
+#define KNIGHT_DEAD_FRAME_1_FILEPATH "Resources/res/Knight/Jump (1).png"
+#define KNIGHT_DEAD_FRAME_2_FILEPATH "Resources/res/Knight/Jump (2).png"
+#define KNIGHT_DEAD_FRAME_3_FILEPATH "Resources/res/Knight/Jump (3).png"
+#define KNIGHT_DEAD_FRAME_4_FILEPATH "Resources/res/Knight/Jump (4).png"
+#define KNIGHT_DEAD_FRAME_5_FILEPATH "Resources/res/Knight/Jump (5).png"
+#define KNIGHT_DEAD_FRAME_6_FILEPATH "Resources/res/Knight/Jump (6).png"
+#define KNIGHT_DEAD_FRAME_7_FILEPATH "Resources/res/Knight/Jump (7).png"
+#define KNIGHT_DEAD_FRAME_8_FILEPATH "Resources/res/Knight/Jump (8).png"
+#define KNIGHT_DEAD_FRAME_9_FILEPATH "Resources/res/Knight/Jump (9).png"
+#define KNIGHT_DEAD_FRAME_10_FILEPATH "Resources/res/Knight/Jump (10).png"
+#define KNIGHT_DEAD_ANIMATION_DURATION 1.6f
+#define KNIGHT_DEAD_SPEED 100.0f
