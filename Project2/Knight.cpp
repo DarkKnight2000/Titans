4ada6bddbc6b667d6a9ec289316e3548b1208eb7
Siddharth0827 <46277902+Siddharth0827@users.noreply.github.com>
2019-02-15 22:15:24 +0530

    Update Knight.cpp

--- Project2/Knight.cpp
+++ Project2/Knight.cpp
@@ -5,16 +5,36 @@ namespace Titans {
 	Knight::Knight( GameDataRef data ) : _data (data){
 
 		_animationIterator = 0;
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 1"));
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 2"));
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 3"));
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 4"));
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 5"));
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 6"));
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 7"));
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 8"));
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 9"));
-		_animationFrames.push_back(_data->assets.GetTexture("Knight Walk Frame 10"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 1"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 2"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 3"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 4"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 5"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 6"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 7"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 8"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 9"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Run Frame 10"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 1"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 2"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 3"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 4"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 5"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 6"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 7"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 8"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 9"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Attack Frame 10"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 1"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 2"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 3"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 4"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 5"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 6"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 7"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 8"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 9"));
+		_animationFrames.push_back(_data->assets.GetTexture("Knight Dead Frame 10"));
 
 		_knightSprite.setTexture( _animationFrames.at(_animationIterator) );
 	}
@@ -23,8 +43,24 @@ namespace Titans {
 		_data->window.draw(_knightSprite);
 	}
 
-	void Knight::walkAnimate(float dt) {
-		if (_clock.getElapsedTime().asSeconds() > KNIGHT_WALK_ANIMATION_DURATION/ _animationFrames.size()) {
+	void Knight::runAnimate(float dt) {
+		if (_clock.getElapsedTime().asSeconds() > KNIGHT_RUN_ANIMATION_DURATION/ _animationFrames.size()) {
+			_animationIterator = (_animationIterator + 1) % _animationFrames.size();
+			_knightSprite.setTexture(_animationFrames.at(_animationIterator));
+			_clock.restart();
+		}
+		_knightSprite.move(KNIGHT_RUN_SPEED * dt, 0);
+	}
+	void Knight::attackAnimate(float dt) {
+		if (_clock.getElapsedTime().asSeconds() > KNIGHT_ATTACK_ANIMATION_DURATION/ _animationFrames.size()) {
+			_animationIterator = (_animationIterator + 1) % _animationFrames.size();
+			_knightSprite.setTexture(_animationFrames.at(_animationIterator));
+			_clock.restart();
+		}
+		_knightSprite.move(KNIGHT_WALK_SPEED * dt, 0);
+	}
+	void Knight::deadAnimate(float dt) {
+		if (_clock.getElapsedTime().asSeconds() > KNIGHT_DEAD_ANIMATION_DURATION/ _animationFrames.size()) {
 			_animationIterator = (_animationIterator + 1) % _animationFrames.size();
 			_knightSprite.setTexture(_animationFrames.at(_animationIterator));
 			_clock.restart();
